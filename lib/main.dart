8555a2bdcf8a814f73810667108165d53f486b27
Huynh Van Huu An <huynhvahuuan3620@gmail.com>
2023-02-26 02:23:53 +0700

    fix register, profile on web

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:movie_ticket_booking_flutter_nlu/config/routes.dart';
 import 'package:movie_ticket_booking_flutter_nlu/screen/profile/profile_screen.dart';
-import 'package:size_config/size_config.dart';
 
 import 'config/theme.dart';
 
@@ -18,18 +17,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return SizeConfigInit(
-      referenceHeight: 900,
-      referenceWidth: 360,
-      builder: (context, _) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        themeMode: ThemeMode.system,
-        theme: themeData(),
-        darkTheme: darkThemeData(),
-        title: 'Movie Ticket Booking',
-        routes: routes,
-        initialRoute: ProfileScreen.routeName,
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: themeData(),
+      darkTheme: darkThemeData(),
+      title: 'Movie Ticket Booking',
+      routes: routes,
+      initialRoute: ProfileScreen.routeName,
     );
   }
 }
